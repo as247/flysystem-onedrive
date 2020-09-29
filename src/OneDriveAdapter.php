f189321ee524546dfaf0db2854fd7457fcb27aa2
as247 <as247@vui360.com>
2020-09-29 18:44:59 +0700

    Update new structure

--- src/OneDriveAdapter.php
+++ src/OneDriveAdapter.php
@@ -8,25 +8,27 @@
 
 namespace As247\Flysystem\OneDrive;
 
-use As247\Flysystem\DriveSupport\Support\DriverForAdapter;
+
+use As247\CloudStorages\Storage\OneDrive;
+use As247\Flysystem\DriveSupport\StorageToAdapter;
 use Microsoft\Graph\Graph;
 use League\Flysystem\Adapter\AbstractAdapter;
 
 class OneDriveAdapter extends AbstractAdapter
 {
-	use DriverForAdapter;
-	protected $driver;
+	use StorageToAdapter;
+	protected $storage;
     public function __construct(Graph $graph, $options = '')
     {
         if(!is_array($options)){
             $options=['root'=>$options];
         }
-    	$this->driver=new Driver($graph,$options);
+    	$this->storage=new OneDrive($graph,$options);
     	$this->setPathPrefix($options['root']??'');
 		$this->throwException=$options['debug']??'';
     }
 	public function getTemporaryUrl($path, $expiration=null, $options=[]){
-		return $this->getMetadata($path)['downloadUrl']??'';
+		return $this->getMetadata($path)['@downloadUrl']??'';
 	}
 
 }
