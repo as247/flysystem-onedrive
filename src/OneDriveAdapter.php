e319ae6164ffd83995231df83f6a88580fda9caa
as247 <as247@vui360.com>
2024-01-31 22:49:44 +0700

    v2.0 which support league/flysystem 2.x

--- src/OneDriveAdapter.php
+++ src/OneDriveAdapter.php
@@ -11,20 +11,22 @@ namespace As247\Flysystem\OneDrive;
 
 use As247\CloudStorages\Storage\OneDrive;
 use As247\Flysystem\DriveSupport\StorageToAdapter;
+use League\Flysystem\FilesystemAdapter;
+use League\Flysystem\PathPrefixer;
 use Microsoft\Graph\Graph;
-use League\Flysystem\Adapter\AbstractAdapter;
 
-class OneDriveAdapter extends AbstractAdapter
+class OneDriveAdapter implements FilesystemAdapter
 {
 	use StorageToAdapter;
 	protected $storage;
+    protected $prefixer;
     public function __construct(Graph $graph, $options = '')
     {
         if(!is_array($options)){
             $options=['root'=>$options];
         }
     	$this->storage=new OneDrive($graph,$options);
-    	$this->setPathPrefix($options['root']??'');
+        $this->prefixer = new PathPrefixer($options['root']??'', DIRECTORY_SEPARATOR);
 		$this->throwException=$options['debug']??'';
     }
 	public function getTemporaryUrl($path, $expiration=null, $options=[]){
